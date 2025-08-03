f3a0e3b72c25feff6a9632de2cf2c1f86493e5da
Prashant Shukla <prashantshukla1029@gmail.com>
2025-08-03 10:37:12 +0530

    Initial Spring Boot + Elasticsearch setup

---
+++ src/main/java/com/assignment/elasticsearch_course_search/ElasticsearchCourseSearchApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.elasticsearch_course_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchCourseSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchCourseSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/elasticsearch_course_search/ElasticsearchCourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.elasticsearch_course_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchCourseSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
